e71f707b14b93d4bcd64885e458f056febf5feff
Isabella Abatgis and Rose Sponseller <isabella+rose@>
2017-06-07 16:42:33 -0700

    did alot of stuff

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(".burger-time").sideNav();
+  $('.sameheight1').matchHeight();
+  $('.sameheight2').matchHeight();
+  $('.sameheight3').matchHeight();
+  $('.sameheight4').matchHeight();
+  $('.drop1').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 18, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+  });
+  $('.drop2').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 18, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: true // Stops event propagation
+  }
+);
+});
